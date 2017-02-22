780a1d522ce490def6a7884c09123279bd947566
blooies <joy28@msn.com>
2017-02-22 00:43:02 -0500

    test

--- components/Project.jsx
+++ components/Project.jsx
@@ -4,10 +4,10 @@ class Project extends Component {
 	render() {
 		return (
 			<div className='project'>
-				<div className='overlay'>
-					<a href={this.props.link} target='_blank'>{this.props.name}</a>
-				</div>
-				<img src={this.props.source}></img>
+				<a href={this.props.link} target='_blank'>
+					<img src={this.props.source}></img>
+				</a>
+				<div className='overlay'><span>{this.props.name}</span></div>
 			</div>
 		)
 	}
