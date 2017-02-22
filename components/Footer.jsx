dcde194a5dc7036ba29dcae6d578db4b9119a84a
Beverly Mah <joy28@msn.com>
2017-02-22 11:20:26 -0500

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+	render() {
+		return (
+			<div className='footer'>
+				<p>I like to work in JavaScript and SCSS. ExtJS, React, Meteor, and Node.</p>
+				<a href="mailto:beverlymah@gmail.com">Contact me!</a>
+			</div>
+		)
+	}
+}
+
+export default Footer;
