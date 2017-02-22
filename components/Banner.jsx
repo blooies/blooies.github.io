20e497e8f54760e648b16b23b005467b3fb124db
blooies <joy28@msn.com>
2017-02-22 00:18:58 -0500

    added banner component

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,1 +1,20 @@
-banner
+import React, { Component } from 'react';
+
+class Banner extends Component {
+	render() {
+		return (
+			<div>
+				<div id='skillz'>
+					<a href='http://www.github.com/blooies' target='_blank'>github</a>
+					<a href='http://www.linkedin.com/bevmah' target='_blank'>linkedin</a>
+				</div>
+				<div id='banner'>
+					<bev>Beverly Mah,</bev>
+					<engineer>Front End Engineer.</engineer>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Banner;
