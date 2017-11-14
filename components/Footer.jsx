74c4efc9ad29bfa6aa090b3715bb6ec17053ada7
blooies <joy28@msn.com>
2017-11-13 20:45:20 -0500

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ class Footer extends Component {
 	render() {
 		return (
 			<div className='footer'>
-				<p>I like to work in JavaScript and SCSS. ExtJS, React, Meteor, and Node.</p>
+				<p>I like to work in JavaScript and LESS/SCSS. React, ExtJS, Meteor, Redux, TypeScript and Node.</p>
 				<a href="mailto:beverlymah@gmail.com">Contact me!</a>
 			</div>
 		)
